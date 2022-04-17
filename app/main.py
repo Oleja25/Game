82a560665801070ce15198733589fb0bd35e9432
Oleja <OlejaMuha@gmail.com>
2022-04-17 18:28:23 +0300

    Nice

--- app/main.py
+++ app/main.py
@@ -1,5 +1,5 @@
 import sys
-from PyQt6.QtCore import QUrl
+from PyQt6.QtCore import QUrl, QSize
 from PyQt6.QtWidgets import QApplication
 from PyQt6.QtQuick import QQuickView
 
@@ -8,7 +8,13 @@ if __name__ == '__main__':
     app = QApplication(sys.argv)
     view = QQuickView()
     view.setSource(QUrl('main.qml'))
+    view.setMaximumWidth(720)
+    view.setMaximumHeight(480)
     view.show()
 
     app.exec()
+    sys.exit()
+
+else:
+
     sys.exit()
--- app/main.qml
+++ app/main.qml
@@ -2,7 +2,20 @@ import QtQuick 2.0
 
 Rectangle {
     id: main
-    width: 250; height: 250
-    color:"green"
-    radius: 7
+    width: 720
+    height: 480
+    color: '#042326'
+
+    Rectangle {
+        //anchors.fill: parent
+        id: op
+        width: 100
+        height: 480
+        color: '#0A3A40'
+    }
+
+    transform: Scale {
+        xScale: main.currentScale
+        yScale: main.currentScale
+    }
 }
